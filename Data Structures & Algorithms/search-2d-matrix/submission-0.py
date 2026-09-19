f27b75481234f70470499a4913f1820635fb1d16
Chaitanya Nawale <chaitanya6.nawale@gmail.com>
2026-09-19 11:16:41 +0200

    Add: search-2d-matrix - submission-0

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-0.py
@@ -0,0 +1,30 @@
+class Solution:
+    def findMid(self, nums: List[int], target):
+        low = 0
+        high = len(nums) - 1
+        while low <= high:
+            mid = ((high - low) // 2) + low
+            if nums[mid] == target:
+                return True
+            elif nums[mid] > target:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return False
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        low = 0
+        high = len(matrix) - 1
+        index = 0
+        while low <= high:
+            mid = ((high - low) // 2) + low
+            matrix[mid][0]
+            if matrix[mid][0] == target:
+                return True
+            elif matrix[mid][0] < target and ( (mid == len(matrix) - 1 ) or (matrix[mid + 1][0] > target ) ):
+                index = mid
+                break
+            elif matrix[mid][0] > target:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return self.findMid(matrix[index], target)
